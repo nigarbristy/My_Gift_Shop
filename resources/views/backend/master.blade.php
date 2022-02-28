f563b0846d96b993e0fa1b9fd1cfe5e22b82863f
nigarbristy <nigar.sultana.bristy01@gmail.com>
2022-03-01 00:03:17 +0600

    change

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -49,13 +49,13 @@
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
-  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="{{url('backend/css/nucleo-icons.css')}}" rel="stylesheet" />
+  <link href="{{url('backend/css/nucleo-svg.css')}}" rel="stylesheet" />
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="{{url('backend/css/nucleo-svg.css')}}" rel="stylesheet" />
   <!-- CSS Files -->
-  <link id="pagestyle" href="../assets/css/soft-ui-dashboard.min.css?v=1.0.3" rel="stylesheet" />
+  <link id="pagestyle" href="{{url('backend/css/soft-ui-dashboard.min.css')}}" rel="stylesheet" />
   <!-- Anti-flicker snippet (recommended)  -->
   <style>
     .async-hide {
